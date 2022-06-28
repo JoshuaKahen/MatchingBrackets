fe76e85392bfd4cbf2c82c8540544b78681735df
JoshuaKahen <67845165+JoshuaKahen@users.noreply.github.com>
2022-06-28 11:43:56 -0400

    Reworked app.js
    Added bracks to keep a record of the bracks that were checked in the template
    Rewrote how back brackets are checked by replacing case with if statement
    Got rid of counter variable
    Switched count and result variable
    Added function checkVal which takes in the true checkboxes

--- app.js
+++ app.js
@@ -1,59 +1,87 @@
 //this is defining the app module and the balancedBracecesTest directive within that module
 angular.module('app', [])
 .controller('Controller', ['$scope', function($scope) {
-  $scope.count = "";
-    $scope.myFunc = function(bracesArr) {
-      
-        let stack = [];
-        let counter = 0;
   
+  $scope.result = " ";
+  
+  $scope.count = [];
+  
+  $scope.bracks = [
+     { id: 1, value: '[]', name: '[square]'}, 
+     { id: 2, value: '{}', name: '{curly}'}, 
+     { id: 3, value: '()', name: '(normal)'}, 
+    ];
+    
+    $scope.myFunc = function(bracesArr) {
+        
+        $scope.checkVal();
+        let tempGroup = $scope.count;
+        const frontMap = new Map();
+        const backMap = new Map();
+        
+        for(let i = 0; i < tempGroup.length ; i++){
+            frontMap.set(tempGroup[i][0], tempGroup[i][1]);
+            backMap.set(tempGroup[i][1], tempGroup[i][0]);
+        }
+        
+        let stack = [];
+
+        if (frontMap.size == 0){
+            $scope.result = "Please check a box";
+            return;
+        }
+
+        
+        if((bracesArr == null) || (bracesArr == "")){
+            $scope.result = "Please enter a String";
+            return;
+        }
+
+        
         for(let i = 0; i < bracesArr.length; i++)
         {
             let x = bracesArr[i];
   
-            if (x == '(' || x == '[' || x == '{')
+            if (frontMap.get(x) != null)
             {
-              
                 stack.push(x);
-                counter = counter + 1;
                 continue;
             }
-              
+            
             let check;
-            switch (x){
-            case ')':
+            
+            if(backMap.get(x) != null){
                 check = stack.pop();
-                counter = counter - 1;
-                if (check == '{' || check == '['){
-                    $scope.count = "This is not balanced";
-			return;}
-                break;
-  
-            case '}':
-                check = stack.pop();
-                counter = counter - 1;
-                if (check == '(' || check == '['){
-                    $scope.count = "This is not balanced";
-			return;}
-                break;
-  
-            case ']':
-                check = stack.pop();
-                counter = counter - 1;
-                if (check == '(' || check == '{'){
-                    $scope.count = "This is not balanced";
-			return;}
-                break;
+                
+                if (check != backMap.get(x)){
+                    $scope.result = "This is not balanced";
+                    return;
+                }
             }
         }
-        if ((stack.length == 0) && (counter == 0))
-            $scope.count = "This is balanced";
+
+        if ((stack.length == 0))
+            $scope.result = "This is balanced";
         else
-            $scope.count = "This is not balanced";
+            $scope.result = "This is not balanced";
     };
+    
     $scope.changeFace = function() {
-        $scope.count = "";
+        $scope.result = " ";
     };
+
+    $scope.checkVal = function(){
+        $scope.count = [];
+        var checkedUsers = '';
+        $scope.bracks.forEach(function(brack) {
+
+          if (brack.selected) {
+            vals = [brack.value[0], brack.value[1]]
+            $scope.count.push(vals);
+          }
+        });
+   }
+ 
   
 }])
 .directive('balancedBracesTest', function () {
@@ -63,3 +91,4 @@ angular.module('app', [])
 		templateUrl: 'balanced-braces.tmpl.html'
 	};
 });
+
